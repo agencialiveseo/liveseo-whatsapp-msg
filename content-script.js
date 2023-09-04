a8eeddbfa46cae7b9afb13efd93432224f3f2d7c
edgarliveseo <edgar@liveseo.com.br>
2023-09-04 16:21:35 -0300

    new functions to add element

--- content-script.js
+++ content-script.js
@@ -1,3 +1,11 @@
+function waitElementOnScreen(selector, callback, interval = 200)
+  {let ckInterval = setInterval(function(){
+    if(document.querySelectorAll(selector).length > 0) {
+      callback()
+      clearInterval(ckInterval);
+    }
+  }, interval);
+}
 
 var observeDOM = (function(){
     var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
@@ -20,7 +28,35 @@ var observeDOM = (function(){
         obj.addEventListener('DOMNodeRemoved', callback, false)
       }
     }
-  })();
+  }
+)();
+
+function addMenuOnFooter(wppMain) {
+  if(wppMain && !wppMain.querySelector('footer').childNodes.length ){
+    
+  const newElement = createNewElement()
+
+  let mySpan = wppMain.querySelector('footer + span > div > span')
+  if(mySpan.nextElementSibling.id == 'send-to-app') return
+  
+    wppMain.querySelector('footer + span > div').insertBefore(newElement, mySpan.nextSibling)
+  }
+}
+
+function createNewElement() {
+  const newAction = document.createElement('span')
+  newAction.textContent = 'Enviar para o app'
+  newAction.id = 'send-to-app'
+  newAction.style.cssText = 'cursor: pointer; margin: 1rem;'
+  newAction.addEventListener('click', () => console.log('clicou'))
+  return newAction
+}
+
+waitElementOnScreen(
+  '#main', 
+  () => observeDOM(document.getElementById('app'), () => addMenuOnFooter(document.getElementById('main')))
+)
+
   
 function debounce(func, timeout = 300){
   let timer;
@@ -31,64 +67,70 @@ function debounce(func, timeout = 300){
 }
   
 function addMenuToApp(mutation, observer){
+  console.log(mutation)
     // precisa arrumar em clicks em conversas ou canto direito superior
-    let mutatedChild = mutation[0].target;
+    // let mutatedChild = mutation[0].target;
     
-    if(mutatedChild.nodeName != "SPAN") return;
+    // if(mutatedChild.nodeName != "SPAN") return;
     
-    let lis = mutatedChild.childNodes[0].childNodes[0].childNodes[0];
+    // let lis = mutatedChild.childNodes[0].childNodes[0].childNodes[0];
 
-    let li = lis.childNodes[0];
+    // let li = lis.childNodes[0];
 
-    li.addEventListener("click", function(event){
-        console.log("cricou");
-    })
+    // li.addEventListener("click", function(event){
+    //     console.log("cricou");
+    // })
     
-    let div = li.childNodes[0];
+    // let div = li.childNodes[0];
 
-    div = div.cloneNode();
-    div.textContent = "Enviar para o app";
-    li = li.cloneNode();
+    // div = div.cloneNode();
+    // div.textContent = "Enviar para o app";
+    // li = li.cloneNode();
 
-    li.appendChild(div);
+    // li.appendChild(div);
     
-    lis.appendChild(li)
+    // lis.appendChild(li)
 }
-  
-observeDOM(document.querySelector("#app > div > span:nth-child(4)"), debounce(addMenuToApp, 200));
+
+
+
+//observeDOM(document.querySelector("#app"), debounce(addMenuToApp, 200));
+
+//observeDOM(document.getElementById('main'), () => addMenuOnFooter(document.getElementById('main')));  
+
   
   ///////////////////////////
   // mensagens selecionadas:
   ///////////////////////////
   
-let itens = document.querySelectorAll("div[role='application'] div[role='row']")
-let selected = [];
-itens.forEach(function(item){
-    let ckbox = item.querySelector("input[type='checkbox']:checked");
-    if(!ckbox) return;
-    selected.push(item)
-})
+// let itens = document.querySelectorAll("div[role='application'] div[role='row']")
+// let selected = [];
+// itens.forEach(function(item){
+//     let ckbox = item.querySelector("input[type='checkbox']:checked");
+//     if(!ckbox) return;
+//     selected.push(item)
+// })
   
   
-for(let i in selected){
+// for(let i in selected){
     
-    let fragment = selected[i].childNodes[0].childNodes[0].childNodes[1];
+//     let fragment = selected[i].childNodes[0].childNodes[0].childNodes[1];
     
-    if(fragment.childElementCount > 2)
-        fragment = fragment.childNodes[1];
-    else 
-        fragment = fragment.childNodes[0];
+//     if(fragment.childElementCount > 2)
+//         fragment = fragment.childNodes[1];
+//     else 
+//         fragment = fragment.childNodes[0];
     
-    if(fragment.childElementCount > 3)
-        fragment = fragment.childNodes[1];
-    else 
-        fragment = fragment.childNodes[0]
+//     if(fragment.childElementCount > 3)
+//         fragment = fragment.childNodes[1];
+//     else 
+//         fragment = fragment.childNodes[0]
     
-    message = fragment.childNodes[0].childNodes[0];
+//     message = fragment.childNodes[0].childNodes[0];
     
-    time = message.getAttribute("data-pre-plain-text").replace(/\[(.*), (.*)\] .*: /, "$2 $1");
-    messageText = message.querySelector(".selectable-text").textContent;
-    name = message.getAttribute("data-pre-plain-text").replace(/\[.*\] (.*): /, "$1");
+//     time = message.getAttribute("data-pre-plain-text").replace(/\[(.*), (.*)\] .*: /, "$2 $1");
+//     messageText = message.querySelector(".selectable-text").textContent;
+//     name = message.getAttribute("data-pre-plain-text").replace(/\[.*\] (.*): /, "$1");
 
-    console.log(time, name, messageText)
-}
+//     console.log(time, name, messageText)
+// }
