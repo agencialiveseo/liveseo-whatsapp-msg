81414003552db2d5c72e924daed3b7fe5d5961e1
edgarliveseo <edgar@liveseo.com.br>
2023-09-05 15:46:44 -0300

    update new modal

--- content-script.js
+++ content-script.js
@@ -34,7 +34,7 @@ var observeDOM = (function(){
 function addMenuOnFooter(wppMain) {
   if(wppMain && !wppMain.querySelector('footer').childNodes.length ){
     
-  const newElement = createNewElement()
+  const newElement = createSendToApp()
 
   let mySpan = wppMain.querySelector('footer + span > div > span')
   if(mySpan.nextElementSibling.id == 'send-to-app') return
@@ -43,18 +43,21 @@ function addMenuOnFooter(wppMain) {
   }
 }
 
-function createNewElement() {
+function createSendToApp() {
   const newAction = document.createElement('span')
   newAction.textContent = 'Enviar para o app'
   newAction.id = 'send-to-app'
   newAction.style.cssText = 'cursor: pointer; margin: 1rem;'
-  newAction.addEventListener('click', () => console.log('clicou'))
+  newAction.addEventListener('click', () => getSelectedMessages())
   return newAction
 }
 
 waitElementOnScreen(
   '#main', 
-  () => observeDOM(document.getElementById('app'), () => addMenuOnFooter(document.getElementById('main')))
+  () => {
+    let newModal = createDialog()
+    document.getElementById('app').appendChild(newModal)
+    observeDOM(document.getElementById('app'), () => addMenuOnFooter(document.getElementById('main')))}
 )
 
   
@@ -103,34 +106,72 @@ function addMenuToApp(mutation, observer){
   // mensagens selecionadas:
   ///////////////////////////
   
-// let itens = document.querySelectorAll("div[role='application'] div[role='row']")
-// let selected = [];
-// itens.forEach(function(item){
-//     let ckbox = item.querySelector("input[type='checkbox']:checked");
-//     if(!ckbox) return;
-//     selected.push(item)
-// })
-  
-  
-// for(let i in selected){
-    
-//     let fragment = selected[i].childNodes[0].childNodes[0].childNodes[1];
-    
-//     if(fragment.childElementCount > 2)
-//         fragment = fragment.childNodes[1];
-//     else 
-//         fragment = fragment.childNodes[0];
-    
-//     if(fragment.childElementCount > 3)
-//         fragment = fragment.childNodes[1];
-//     else 
-//         fragment = fragment.childNodes[0]
-    
-//     message = fragment.childNodes[0].childNodes[0];
-    
-//     time = message.getAttribute("data-pre-plain-text").replace(/\[(.*), (.*)\] .*: /, "$2 $1");
-//     messageText = message.querySelector(".selectable-text").textContent;
-//     name = message.getAttribute("data-pre-plain-text").replace(/\[.*\] (.*): /, "$1");
+function getSelectedMessages() {
+  let itens = document.querySelectorAll("div[role='application'] div[role='row']")
+  let selected = [];
+  itens.forEach(function(item){
+      let ckbox = item.querySelector("input[type='checkbox']:checked");
+      if(!ckbox) return;
+      selected.push(item)
+  })
 
-//     console.log(time, name, messageText)
-// }
+  let selectedMessages = []
+
+  for(let i in selected){
+    let message = selected[i].querySelector('.copyable-text')
+    let time = message.getAttribute("data-pre-plain-text").replace(/\[(.*), (.*)\] .*: /, "$2 $1");
+    let messageText = message.querySelector(".selectable-text").textContent;
+    let name = message.getAttribute("data-pre-plain-text").replace(/\[.*\] (.*): /, "$1");
+    let img = ''
+    // const messageImgs = message.querySelectorAll("img")
+    // if(messageImgs.length > 0) {
+    //   img = messageImgs[1].getAttribute('src')
+    // }
+
+    selectedMessages.push({time, name, messageText})
+  }
+  console.log(selectedMessages)
+  createMessages(selectedMessages)
+  myDialog.showModal()
+}
+
+function createDialog() {
+  const dialog = document.createElement('dialog')
+  const header = document.createElement('header')
+  dialog.id = 'myDialog'
+  header.id = 'myDialog-header'
+  header.textContent = 'teste'
+  dialog.appendChild(header)
+  dialog.appendChild(createDialogFooter())
+  return dialog
+}
+
+function createDialogFooter() {
+  const footer = document.createElement('footer')
+  footer.id = 'myDialog-footer'
+  footer.appendChild(createCloseDialog())
+  return footer
+}
+
+function createCloseDialog() {
+  const closeButton = document.createElement('button')
+  closeButton.id = 'close-button'
+  closeButton.textContent = 'Fechar'
+  closeButton.addEventListener('click', () => myDialog.close())
+  return closeButton
+}
+
+function createMessages(selectedMessages) {
+  for(let item of selectedMessages) {
+    var messageBody = document.createElement('div')
+    messageBody.className = 'selected-message'
+    var messageOrigin = document.createElement('div')
+    var messageText = document.createTextNode(item.messageText)
+    messageOrigin.innerHTML = item.time + ' - ' + item.name
+    messageBody.appendChild(messageOrigin)
+    messageBody.appendChild(messageText)
+    var dialog = document.getElementById('myDialog')
+    var header = dialog.firstChild
+    header.after(messageBody)
+  }
+}
